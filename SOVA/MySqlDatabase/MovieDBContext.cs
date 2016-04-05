864ba4b715508e3da4615570b629ea32e2ce3ee2
jonasschjerlund <jonassc@ruc.dk>
2016-04-05 11:18:55 +0200

    Removed redundant using statements. Generally just testing connection

--- SOVA/MySqlDatabase/MovieDBContext.cs
+++ SOVA/MySqlDatabase/MovieDBContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using DomainModel;
 
 namespace MySqlDatabase
@@ -14,6 +9,7 @@ namespace MySqlDatabase
         {
 
         }
+
         /******************************************
             StackOverflow tabels
         ******************************************/
