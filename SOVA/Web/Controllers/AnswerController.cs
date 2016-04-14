012e52f4e9f03870ec02d8b0c0cdf68118834d5a
madssk <mads.saustrup.kristensen@gmail.com>
2016-04-14 15:22:13 +0200

    Merge error fix

---
+++ SOVA/Web/Controllers/AnswerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Web.Http;
+using DataAccessLayer;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AnswersController : BaseApiController
+    {
+        private readonly IRepository _repository = new MySqlRepository();
+
+        public IHttpActionResult Get()
+        {
+            var comments = _repository.GetComments().Select(p => ModelFactory.Map(p, Url));
+
+            var result = GetAll(comments);
+
+            return Ok(result);
+        }
+
+        public IHttpActionResult Get(string searchString)
+        {
+            var comments = _repository.GetComments(searchString).Select(p => ModelFactory.Map(p, Url));
+
+            var result = GetAll(comments);
+
+            return Ok(result);
+        }
+
+
+        public IHttpActionResult Get(int id)
+        {
+            var commentModel = ModelFactory.Map(_repository.FindComment(id), Url);
+
+            if (commentModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(commentModel);
+        }
+    }
+}
