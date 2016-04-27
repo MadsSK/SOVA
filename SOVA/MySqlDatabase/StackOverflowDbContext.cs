496fc1c031a00620fbf56b0dfd1b107b005dd1e2
madssk <mads.saustrup.kristensen@gmail.com>
2016-04-27 17:19:28 +0200

    Recodede modelbuilder One-To-Many, Many-To-Many & Many-To-One

--- SOVA/MySqlDatabase/StackOverflowDbContext.cs
+++ SOVA/MySqlDatabase/StackOverflowDbContext.cs
@@ -44,7 +44,16 @@ namespace MySqlDatabase
             modelBuilder.Entity<Annotation>()
                 .Property(a => a.SearchUserId)
                 .HasColumnName("search_user_id");
-            
+            //Many-To-One
+            modelBuilder.Entity<Annotation>()
+                .HasOptional<Post>(a => a.Post)
+                .WithMany(p => p.Annotations);
+            modelBuilder.Entity<Annotation>()
+                .HasOptional<Comment>(a => a.Comment)
+                .WithMany(c => c.Annotations);
+            modelBuilder.Entity<Annotation>()
+                .HasRequired<SearchUser>(a => a.SearchUser)
+                .WithMany(s => s.Annotations);
 
             /*****************************
                 Answer
@@ -53,7 +62,11 @@ namespace MySqlDatabase
             modelBuilder.Entity<Answer>().ToTable("answers");
             modelBuilder.Entity<Answer>().Property(a => a.QuestionId)
                 .HasColumnName("parentId");
-            
+            //Many-To-One
+            modelBuilder.Entity<Answer>()
+                .HasRequired(a => a.Question)
+                .WithMany(q => q.Answers);
+
             /*****************************
                 Comment
             *****************************/
@@ -64,20 +77,74 @@ namespace MySqlDatabase
             modelBuilder.Entity<Comment>().Property(c => c.Body).HasColumnName("commenttext");
             modelBuilder.Entity<Comment>().Property(c => c.CreateDate).HasColumnName("commentcreatedate");
             modelBuilder.Entity<Comment>().HasKey(c => c.Id);
-            
+            //One-To-Many
+            modelBuilder.Entity<Comment>()
+                .HasMany<Annotation>(c => c.Annotations)
+                .WithOptional(a => a.Comment);
+            //Many-To-One
+            modelBuilder.Entity<Comment>()
+                .HasRequired<User>(c => c.User)
+                .WithMany(q => q.Comments);
+            modelBuilder.Entity<Comment>()
+                .HasRequired<Post>(c => c.Post)
+                .WithMany(q => q.Comments);
+
             /*****************************
                 Post
             *****************************/
             //Renaming
             modelBuilder.Entity<Post>().ToTable("posts");
             modelBuilder.Entity<Post>().Property(p => p.UserId).HasColumnName("owneruserid");
-            
+            //One-To-Many
+            modelBuilder.Entity<Post>()
+                .HasMany<Comment>(p => p.Comments)
+                .WithRequired(c => c.Post);
+            modelBuilder.Entity<Post>()
+                .HasMany<Annotation>(p => p.Annotations)
+                .WithOptional(a => a.Post);
+            //Many-To-Many
+            modelBuilder.Entity<Post>()
+                .HasMany<Tag>(p => p.Tags)
+                .WithMany(t => t.Posts)
+                .Map(tp =>
+                {
+                    tp.MapLeftKey("tag_id");
+                    tp.MapRightKey("post_id");
+                    tp.ToTable("tags_posts");
+                });
+            modelBuilder.Entity<Post>()
+                .HasMany<SearchUser>(p => p.SearchUsers)
+                .WithMany(s => s.Posts)
+                .Map(tp =>
+                {
+                    tp.MapLeftKey("post_id");
+                    tp.MapRightKey("search_user_id");
+                    tp.ToTable("favorites");
+                });
+            modelBuilder.Entity<Post>()
+                .HasMany<Post>(p => p.LinkedPosts)
+                .WithMany()
+                .Map(tp =>
+                {
+                    tp.MapLeftKey("linkpostid");
+                    tp.MapRightKey("id");
+                    tp.ToTable("linkedposts");
+                });
+            //Many-To-One
+            modelBuilder.Entity<Post>()
+                .HasRequired<User>(p => p.User)
+                .WithMany(u => u.Posts);
+
             /*****************************
                 Question
             *****************************/
             //Renaming
             modelBuilder.Entity<Question>().ToTable("questions");
-            
+            //One-To-Many
+            modelBuilder.Entity<Question>()
+                .HasMany<Answer>(q => q.Answers)
+                .WithRequired(a => a.Question);
+
             /*****************************
                 Search
             *****************************/
@@ -87,7 +154,11 @@ namespace MySqlDatabase
             modelBuilder.Entity<Search>().Property(s => s.SearchUserId).HasColumnName("search_user_id");
             modelBuilder.Entity<Search>().Property(s => s.SearchString).HasColumnName("search_string");
             modelBuilder.Entity<Search>().Property(s => s.DateTime).HasColumnName("search_date_time");
-            
+            //Many-To-One
+            modelBuilder.Entity<Search>()
+                .HasRequired<SearchUser>(s => s.SearchUser)
+                .WithMany(su => su.Searches);
+
             /*****************************
                 SearchUser
             *****************************/
@@ -97,7 +168,25 @@ namespace MySqlDatabase
             modelBuilder.Entity<SearchUser>()
                 .Property(sh => sh.MacAdresse)
                 .HasColumnName("mac_adresse");
-            
+            //One-To-Many
+            modelBuilder.Entity<SearchUser>()
+                .HasMany<Annotation>(su => su.Annotations)
+                .WithRequired(a => a.SearchUser);
+            //One-To-Many
+            modelBuilder.Entity<SearchUser>()
+                .HasMany<Search>(su => su.Searches)
+                .WithRequired(s => s.SearchUser);
+            //Many-To-Many
+            modelBuilder.Entity<SearchUser>()
+                .HasMany<Post>(su => su.Posts)
+                .WithMany(p => p.SearchUsers)
+                .Map(f =>
+                {
+                    f.MapLeftKey("post_id");
+                    f.MapRightKey("search_user_id");
+                    f.ToTable("favorites");
+                });
+
             /*****************************
                 Tag
             *****************************/
@@ -105,7 +194,18 @@ namespace MySqlDatabase
             modelBuilder.Entity<Tag>().ToTable("tags");
             modelBuilder.Entity<Tag>().Property(t => t.Id).HasColumnName("tag_id");
             modelBuilder.Entity<Tag>().Property(t => t.Body).HasColumnName("tag");
-            
+            //Many-To-Many
+            modelBuilder.Entity<Tag>()
+                .HasMany<Post>(t => t.Posts)
+                .WithMany(p => p.Tags)
+                .Map(f =>
+                {
+                    f.MapLeftKey("tag_id");
+                    f.MapRightKey("post_id");
+                    f.ToTable("tags_posts");
+                });
+
+
             /*****************************
                 User
             *****************************/
@@ -116,7 +216,13 @@ namespace MySqlDatabase
             modelBuilder.Entity<User>().Property(u => u.CreationDate).HasColumnName("usercreationdate");
             modelBuilder.Entity<User>().Property(u => u.Location).HasColumnName("userlocation");
             modelBuilder.Entity<User>().Property(u => u.Age).HasColumnName("userage");
-            
+            //One-To-Many
+            modelBuilder.Entity<User>()
+                .HasMany<Post>(u => u.Posts)
+                .WithRequired(p => p.User);
+            modelBuilder.Entity<User>()
+                .HasMany<Comment>(u => u.Comments)
+                .WithRequired(c => c.User);
 
             base.OnModelCreating(modelBuilder);
         }
