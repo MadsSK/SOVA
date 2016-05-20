ab2eb6ce8a1afefed7dfaa7be6b9e150c1497b33
jonasschjerlund <jonassc@ruc.dk>
2016-05-20 15:39:22 +0200

    Fixed component registration which used to all reigster to questionsComponent (still not functional though!)

--- SOVA/Web/scripts/app/components/topbarmenu/topbarmenuviewmodel.js
+++ SOVA/Web/scripts/app/components/topbarmenu/topbarmenuviewmodel.js
@@ -1,1 +1,12 @@
-﻿
+﻿define(['knockout', 'app/dataservice'], function (ko, dataservice) {
+    return function(params) {
+        var currentComponent = params.currentComponent;
+
+        var menuElements = ko.observableArray();
+
+        return {
+            menuElements: config.menuElements,
+            currentComponent: currentComponent,
+        }
+    };
+});
