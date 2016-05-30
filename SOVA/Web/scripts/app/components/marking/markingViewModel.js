76453ab4897a85f9539a01d16933a047f1be3d3e
madssk <mads.saustrup.kristensen@gmail.com>
2016-05-30 16:00:04 +0200

    Adding a marking component, to show the annotated text

---
+++ SOVA/Web/scripts/app/components/marking/markingViewModel.js
@@ -0,0 +1,13 @@
+﻿define(['knockout', 'app/dataservice', 'app/viewmodel'], function (ko, dataservice, viewmodel) {
+    return function(params) {
+        var marking = ko.observable();
+        var annotation = ko.observable(params);
+        dataservice.getData(annotation().url, function (data) {
+            marking(data.body.substring(annotation().markingStart, annotation().markingEnd - annotation().markingStart));
+        });
+        
+        return {
+            marking: marking
+        }
+    };
+});
